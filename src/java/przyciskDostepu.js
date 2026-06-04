afc80299a7e4c903622961cfeb9fbbe7c719a2a4
Keczuu <151464168+Keczuu@users.noreply.github.com>
2026-06-04 10:12:54 +0200

    naprawienie przycisku dostepu

--- src/java/przyciskDostepu.js
+++ src/java/przyciskDostepu.js
@@ -2,21 +2,20 @@ if (localStorage.getItem('accessibility') === 'enabled') {
     document.body.classList.add('dostepnosc');
 }
 let czas = 0;
-for (const btn of document.querySelectorAll('.accessibility')) { // lecimy po wszysktich, bo możliwe że wstawimy np w headerze i w stopce taki przycisk
-    // po kliknięciu dodajemy/usuwamy klasę `is-dark` na body
-    btn.addEventListener('click', () => {
-        const czasTeraz = Date.now();
-        if (czasTeraz - czas < 1000) {
-            alert("Poczekaj sekunde przed kolejna zmiana");
-            return;
-        }
-        czas = czasTeraz;
-        document.body.classList.toggle('dostepnosc');
+document.addEventListener('click', (e) => { // to musialo byc zmienione bo tak samo jak z ladowaniem gier, strona sie ladowala wolniej od skryptu
+    const btn = event.target.closest('.accessibility'); // tutaj jest target closest bo jakby sie kliknelo w ten przycisk to to by nie dzialalo bo nie klikasz w przycisk tak jakby tylko w literke K wiec dzieki temu to idzie w gore tego drzewa i sprawdza czy jest tak klasa accessibility
+    if (!btn) return;
+    const czasTeraz = Date.now();
+    if (czasTeraz - czas < 1000) {
+        alert("Poczekaj sekunde przed kolejna zmiana"); // to trzeba zmienic na toast
+        return;
+    }
+    czas = czasTeraz;
+    document.body.classList.toggle('dostepnosc');
 
-        if (document.body.classList.contains('dostepnosc')) {
-            localStorage.setItem('accessibility', 'enabled');
-        } else {
-            localStorage.setItem('accessibility', 'disabled');
-        }
-    });
-}
+    if (document.body.classList.contains('dostepnosc')) {
+        localStorage.setItem('accessibility', 'enabled');
+    } else {
+        localStorage.setItem('accessibility', 'disabled');
+    }
+});
